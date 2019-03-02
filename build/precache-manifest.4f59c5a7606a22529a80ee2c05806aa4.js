f9a4cfe274118c35d712ce2a658ec3092c85241c
jwbuiter <jwbuiter@gmail.com>
2019-03-02 16:43:38 +0100

    Update build

---
+++ build/precache-manifest.4f59c5a7606a22529a80ee2c05806aa4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "645b454957c764fce54b602ac3dac76f",
+    "url": "/static/media/Logo-MBDC.645b4549.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b6189becd9c9a192ef96",
+    "url": "/static/js/main.b6189bec.chunk.js"
+  },
+  {
+    "revision": "8090142bfea3c29a091a",
+    "url": "/static/js/1.8090142b.chunk.js"
+  },
+  {
+    "revision": "b6189becd9c9a192ef96",
+    "url": "/static/css/main.5e1b9663.chunk.css"
+  },
+  {
+    "revision": "8090142bfea3c29a091a",
+    "url": "/static/css/1.97ed6d0b.chunk.css"
+  },
+  {
+    "revision": "53eb2c1a2089602aa962e928ec89a3d7",
+    "url": "/index.html"
+  }
+];
