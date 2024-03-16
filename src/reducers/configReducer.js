4aefc0b9b4dc2d5560c619e8b197f60ec3d6e79d
Jan Buiter <jwbuiter@gmail.com>
2024-03-16 11:48:42 +0100

    fix missing config entries causing failure

--- src/reducers/configReducer.js
+++ src/reducers/configReducer.js
@@ -1,18 +1,34 @@
-const {
-  RECEIVE_CONFIG,
-  CONFIG_UNLOCK,
-  CONFIG_LOCK,
-  CONFIG_CHANGE
-} = require("../actions/types");
+const { RECEIVE_CONFIG, CONFIG_UNLOCK, CONFIG_LOCK, CONFIG_CHANGE } = require("../actions/types");
 
 const { set } = require("../helpers");
 
 const initialState = { loaded: false, locked: true, hasChanged: false };
 
-export default function(state = initialState, action) {
+function mergeConfig(template, config) {
+  if (config === undefined) return template;
+  if (template === undefined) return config;
+
+  if (typeof template !== "object") return config;
+
+  let result;
+  if (Array.isArray(template)) {
+    result = [];
+    let longest = Math.max(template.length, config.length);
+    for (let i = 0; i < longest; i++) result[i] = mergeConfig(template[i], config[i]);
+  } else {
+    result = {};
+    for (const key in template) {
+      result[key] = mergeConfig(template[key], config[key]);
+    }
+  }
+
+  return result;
+}
+
+export default function (state = initialState, action) {
   switch (action.type) {
     case RECEIVE_CONFIG: {
-      return { ...state, loaded: true, hasChanged: true, ...action.payload };
+      return { ...action.payload, ...mergeConfig(state, action.payload), loaded: true, hasChanged: true };
     }
     case CONFIG_UNLOCK: {
       return { ...state, locked: false, hasChanged: false };
